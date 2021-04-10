a730113df0102c9e44504f2ae28f07c531894694
Diziet <diziet559@gmail.com>
2021-04-10 21:07:06 +0200

    Added basic dps calculation.
    - simple damage calculation for each shot
    - dps calculated as damage / time
    - display of rotation stats and dps stats next to plot

--- rotationtools.py
+++ rotationtools.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
 class rotationplot:
-    range_speed = 2.9
+    ranged = {'dps': 83.3, 'speed': 2.9, 'ammo_dps': 23,'ap': 2300, 'crit': 30, 'multiplier': 1.02}
+    melee = {'dps': 118.6, 'speed': 3.7, 'ap': 2000, 'crit': 25, 'multiplier': 1.02}
+    
     gcd_available = 0
     auto_available = 0
     current_time = 0
+    total_damage = 0
+    damage = {'auto': 0, 'steady': 0, 'multi': 0, 'arcane': 0, 'raptor': 0, 'melee': 0}
     haste = 1.2 * 1.15 * 1.05
     autocolor = 'firebrick'
     delaycolor = 'lightcoral'
@@ -18,6 +22,18 @@ class rotationplot:
     
     def init_fig(self):
         fig, self.ax = plt.subplots(figsize=(10,5), dpi=150)
+        self.damage['auto'] = ((self.ranged['dps'] + self.ranged['ammo_dps'])*self.ranged['speed'] \
+            + self.ranged['ap']/14*self.ranged['speed']) * self.ranged['multiplier'] * (1 + 1.3 * self.ranged['crit']/100)
+        self.damage['steady'] = (self.ranged['dps']*self.ranged['speed'] +150 \
+            + self.ranged['ap']*0.2) * self.ranged['multiplier'] * (1 + 1.3 * self.ranged['crit']/100)
+        self.damage['multi'] = ((self.ranged['dps'] + self.ranged['ammo_dps'])*self.ranged['speed'] +205 \
+            + self.ranged['ap']*0.2) * self.ranged['multiplier'] * (1 + 1.3 * self.ranged['crit']/100)
+        self.damage['arcane'] = (273 \
+            + self.ranged['ap']*0.15) * self.ranged['multiplier'] * (1 + 1.3 * self.ranged['crit']/100)
+        self.damage['raptor'] = (self.melee['dps']*self.melee['speed'] + 170 \
+            + self.melee['ap']/14*self.melee['speed']) * self.melee['multiplier'] * (1 + 1.3 * self.melee['crit']/100)
+        self.damage['melee'] = (self.melee['dps']*self.melee['speed'] \
+            + self.melee['ap']/14*self.melee['speed']) * self.melee['multiplier'] * (1 + 1.3 * self.melee['crit']/100)
         
     def complete_fig(self):
         self.ax.set_ylim(0, 3)
@@ -29,9 +45,10 @@ class rotationplot:
         labels = list(colors.keys())
         handles = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
         plt.legend(handles, labels, bbox_to_anchor=(1.01, 1), loc='upper left')
-        rota = self.rot_stats.format(speed = self.range_speed, haste = self.haste, dur=self.auto_available)
+        self.dps = self.total_damage / self.auto_available
+        rota = self.rot_stats.format(speed = self.ranged['speed'], haste = self.haste, dur=self.auto_available)
         plt.annotate(rota,(1.01,0.5), xycoords='axes fraction')
-        stats = self.dps_stats.format(rap=2300,map=2000,crit=30,dps=0)
+        stats = self.dps_stats.format(rap=self.ranged['ap'],map=self.melee['ap'],crit=self.ranged['crit'],dps=self.dps)
         plt.annotate(stats,(1.01,0.3), xycoords='axes fraction')
         #plt.annotate('Range haste: '+str(self.haste),(1.01,0.455), xycoords='axes fraction')
         plt.show()
@@ -43,7 +60,8 @@ class rotationplot:
             self.ax.bar(self.auto_available, 0.2, self.current_time-self.auto_available, 0.4, facecolor='white', edgecolor='lightcoral', align='edge')
         self.ax.bar(self.current_time, 0.8, 0.5/self.haste, 0.1, facecolor='white', edgecolor='firebrick', align='edge')
         self.current_time = self.current_time + 0.5/self.haste
-        self.auto_available = self.current_time + (self.range_speed-0.5)/self.haste
+        self.auto_available = self.current_time + (self.ranged['speed']-0.5)/self.haste
+        self.total_damage = self.total_damage + self.damage['auto']
         
     def add_gcd(self):
         self.ax.bar(self.current_time, 0.8, 1.5, 2.1, facecolor='white', edgecolor='black', align='edge')
@@ -55,6 +73,7 @@ class rotationplot:
         self.ax.bar(self.current_time, 0.8, 1.5/self.haste, 1.1, facecolor='white', edgecolor=self.steadycolor, align='edge')
         self.gcd_available = self.current_time + 1.5
         self.current_time = self.current_time + 1.5/self.haste
+        self.total_damage = self.total_damage + self.damage['steady']
 
     def add_multi(self):
         if (self.gcd_available > self.current_time):
@@ -63,6 +82,7 @@ class rotationplot:
         self.ax.bar(self.current_time, 0.8, 0.5/self.haste, 1.1, facecolor='white', edgecolor=self.multicolor, align='edge')
         self.gcd_available = self.current_time +1.5
         self.current_time = self.current_time + 0.5/self.haste
+        self.total_damage = self.total_damage + self.damage['multi']
 
     def add_arcane(self):
         if (self.gcd_available > self.current_time):
@@ -71,10 +91,12 @@ class rotationplot:
         self.ax.bar(self.current_time, 0.8, 0.1, 1.1, facecolor='white', edgecolor=self.arcanecolor, align='edge')
         self.gcd_available = self.current_time + 1.5
         self.current_time = self.current_time + 0.1
+        self.total_damage = self.total_damage + self.damage['arcane']
         
     def add_raptor(self):
         self.ax.bar(self.current_time, 0.8, 0.4, 1.1, facecolor='white', edgecolor=self.raptorcolor, align='edge')
         self.current_time = self.current_time + 0.4
+        self.total_damage = self.total_damage + (self.damage['raptor'] + self.damage['melee'])/2
         
     def add_rotation(self, s):
         for c in s:
@@ -94,6 +116,4 @@ if __name__ == "__main__":
     r = rotationplot()
     r.init_fig()
     r.add_rotation('asAarsasmarsas')
-    print(r.auto_available)
-    print(r.gcd_available)
     r.complete_fig()
